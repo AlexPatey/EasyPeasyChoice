f96a395777259167cb3ef1c5aab3fb88f3312b7d
= <alex.patey@btinternet.com>
2022-11-20 19:53:21 +0000

    kick the drink and easyway added; every link section removed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
-import eazyPeazyLogo from "./images/easypeasy.jpg";
+import easyPeasyLogo from "./images/easypeasy.jpg";
+import kickTheDrinkCover from "./images/kickthedrink.jpg";
+import easyWayCover from "./images/easyway.jpg";
 import downArrow from "./images/arrow-down.png";
 import { Canvas, useFrame, useLoader } from '@react-three/fiber'
 import React, { useRef, useState, useEffect } from 'react'
 import { TextureLoader } from 'three';
 
-function Box(props) {
+function Book(props) {
 
     const ref = useRef()
 
@@ -21,12 +23,12 @@ function Box(props) {
         <mesh
             {...props}
             ref={ref}
-            scale={1.5}
-            onClick={() => window.open("https://easypeasymethod.org/", "_blank")}
+            scale={2}
+            onClick={() => window.open(props.link, "_blank")}
             onPointerOver={(event) => hover(true)}
             onPointerOut={(event) => hover(false)}>
             <boxGeometry args={[1.748, 2.480, 0]} />
-            <meshStandardMaterial map={useLoader(TextureLoader, eazyPeazyLogo)} />
+            <meshStandardMaterial map={useLoader(TextureLoader, props.image)} />
         </mesh>
     )
 }
@@ -34,26 +36,57 @@ function Box(props) {
 function App() {
     return (
         <div className="App">
-            <h1 className='easyTitle'>Make the easy choice</h1>
-            <h2 className='quitText'>Quit porn today</h2>
-            <Canvas className='canvas'>
-                <ambientLight />
-                <pointLight position={[10, 10, 10]} />
-                <Box position={[0, 0, 0]} />
-            </Canvas>
-            <h2 className='orText'>Or...</h2>
-            <button className='goDownBtn' onClick={() => document.getElementById('siteList').scrollIntoView()}>
-                <img className='downArrowImg' src={downArrow} alt="down arrow"></img>
-            </button>
-            <h4>Every **** site ever...</h4>
-            <ul id='siteList' className='siteList'>
-                <li>Site Link</li>
-                <li>Site Link</li>
-                <li>Site Link</li>
-                <li>Site Link</li>
-                <li>Site Link</li>
-                <li>Site Link</li>
-            </ul>
+            <h1 className='easyTitle'>Welcome</h1>
+            <h2 className='quitText'>Everything we know about addiction is wrong (that's okay though!)</h2>
+            <h2 className='quitText'>Free books, with love -- for you, and anyone else</h2>
+            <div className='canvasContainer'>
+                <div className='book'>
+                    {/*<p className='bookName'>Easy Peasy</p>*/}
+                    <Canvas className='canvas' height="calc(100vh - 248px)">
+                        <ambientLight />
+                        <pointLight position={[10, 10, 10]} />
+                        <Book position={[0, 0, 0]} image={easyPeasyLogo} link={"https://easypeasymethod.org/"} />
+                    </Canvas>
+                    {/*<a class="bookLink" target="_blank" href={"https://easypeasymethod.org/"}>Easy Peasy</a>*/}
+                </div>
+                <div className='book'>
+                    {/*<p className='bookName'>Kick the Drink... Easily</p>*/}
+                    <Canvas className='canvas'>
+                        <ambientLight />
+                        <pointLight position={[10, 10, 10]} />
+                        <Book position={[0, 0, 0]} image={kickTheDrinkCover} link={"https://drive.google.com/drive/folders/1XLoabtbRSFSUlDD8rUuN3U__GNzzVY1E"} />
+                    </Canvas>
+                    {/*<a class="bookLink" target="_blank" href={"https://drive.google.com/drive/folders/1XLoabtbRSFSUlDD8rUuN3U__GNzzVY1E"}>Kick the Drink... Easily!</a>*/}
+                </div>
+                <div className='book'>
+                    {/*<p className='bookName'>Easyway</p>*/}
+                    <Canvas className='canvas' height="calc(100vh - 248px)">
+                        <ambientLight />
+                        <pointLight position={[10, 10, 10]} />
+                        <Book position={[0, 0, 0]} image={easyWayCover} link={"https://drive.google.com/drive/folders/1XLoabtbRSFSUlDD8rUuN3U__GNzzVY1E"} />
+                    </Canvas>
+                    {/*<a class="bookLink" target="_blank" href={"https://drive.google.com/drive/folders/1XLoabtbRSFSUlDD8rUuN3U__GNzzVY1E"}>Easyway</a>*/}
+                </div>
+            </div>
+            <h3 className='quitText'>You can help by sharing this page,</h3>
+            <h3 className='quitText'>[[Share links here]]</h3>
+            <h3></h3>
+            <h3 className='quitText'>{"Discussing it on a <new type of computer> at ~mogsub-possum/easily"}</h3>
+            <h3></h3>
+            <h3 className='quitText'>{"And, if you'd like, by joining our campaign for <peaceful plain packaging laws>"}</h3>
+            {/*<h2 className='orText'>Or...</h2>*/}
+            {/*<button className='goDownBtn' onClick={() => document.getElementById('siteList').scrollIntoView()}>*/}
+            {/*    <img className='downArrowImg' src={downArrow} alt="down arrow"></img>*/}
+            {/*</button>*/}
+            {/*<h3>Every **** site ever...</h3>*/}
+            {/*<ul id='siteList' className='siteList'>*/}
+            {/*    <li>Site Link</li>*/}
+            {/*    <li>Site Link</li>*/}
+            {/*    <li>Site Link</li>*/}
+            {/*    <li>Site Link</li>*/}
+            {/*    <li>Site Link</li>*/}
+            {/*    <li>Site Link</li>*/}
+            {/*</ul>*/}
         </div>
     );
 }
